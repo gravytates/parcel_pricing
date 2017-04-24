1a711b4ea400996f6c9a34faad400a39ca05af39
Grady Shelton and Kai de Rochemont <kai@>
2017-04-24 10:57:29 -0700

    pass second unit test calculating shipping cost based on additional parameters including volume, weight, distance and mode and transportation

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,11 +1,37 @@
 class Parcel
-  define_method(:initialize) do |width, length, height|
+  define_method(:initialize) do |width, length, height, weight, delivery_mode, distance|
     @width = width.to_i
     @length = length.to_i
     @height = height.to_i
+    @weight = weight.to_i
+    @delivery_mode = delivery_mode
+    @distance = distance.to_i
   end
 
   define_method(:volume) do
     @width.*(@length).*(@height)
   end
+
+  define_method(:shipping_cost) do
+    cost = 0
+    parcel_volume = self.volume
+    (1..parcel_volume).each do
+      cost += 0.25
+    end
+    (1..@weight).each do
+      cost += 1
+    end
+    if @delivery_mode.==('truck')
+      cost += 1
+    elsif @delivery_mode.==('plane')
+      cost += 6
+    elsif @delivery_mode.==('bike')
+      cost += 10
+    end
+    (1..@distance).each do
+      cost += 0.05
+    end
+    cost
+  end
+
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -4,8 +4,15 @@ require('parcel')
 describe(Parcel) do
   describe('#volume') do
     it('check dimensions to find volume') do
-      new_parcel = Parcel.new(6,5,7)
+      new_parcel = Parcel.new(6,5,7,1,"bike",1)
       expect(new_parcel.volume).to(eq(210))
+    end
+  end
+
+  describe('#shipping_cost') do
+    it('finds price for shipping based on volume and other shippng parameters') do
+      new_parcel = Parcel.new(1,1,1,1,"bike",1)
+      expect(new_parcel.shipping_cost).to(eq(11.3))
     end
   end
 end
